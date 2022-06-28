484fcf9a4ffc040f3b6dac5769effff9a437e9ad
Andrey <andrey@MacBook-Air-Andrey.local>
2022-06-28 17:49:00 +0300

    add react component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Cart from './container/Cart';
 export const App = () => {
   return (
     <div
@@ -10,7 +11,7 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Cart />
     </div>
   );
 };
